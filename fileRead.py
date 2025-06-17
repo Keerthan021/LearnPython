1fd42316ac2327767b7841b2adc20e3a41732eeb
Keerthan <keerthanderla@gmail.com>
2025-06-17 12:45:41 +0530

    branch example

--- fileRead.py
+++ fileRead.py
@@ -4,3 +4,8 @@ print(file.read())
 
 #new changes made here
 print(f'This is new print{file.read()}')
+#branch
+print(f'This is new print{file.read()}')
+print(f'This is new print{file.read()}')
+print(f'This is new print{file.read()}')
+print(f'This is new print{file.read()}')
